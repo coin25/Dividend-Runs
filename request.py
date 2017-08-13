5f249a547d71caa3c61ae463f19bc5c5246c040d
coin25 <azzy_coin25@hotmail.com>
2017-08-13 14:42:32 -0700

    Update with TRI

--- request.py
+++ request.py
@@ -12,24 +12,29 @@ url = 'https://api.intrinio.com/historical_data?identifier=AAPL&&sort_order=asc&
 #url = 'https://api.intrinio.com/prices?identifier=AAPL&sort_order=asc&page_number=1'
 s = requests.Session()
 s.auth = (USER, PASS)
-
-
-
 dividends = json.loads(s.get(url).text)
-print(dividends)
+
+url = 'https://api.intrinio.com/prices?identifier=AAPL&sort_order=asc&page_number=1'
+s = requests.Session()
+s.auth = (USER, PASS)
+r = json.loads(s.get(url).text)
+
+#Reformat to proper dates
 for entry in dividends["data"]:
     year, month, day = entry["date"].split("-")
     entry["date"] = datetime.date(int(year), int(month), int(day))
 
-print(dividends["data"][1]["date"] - dividends["data"][0]["date"])
+
 start = dividends["data"][0]["date"]
 window = datetime.date(start.year + 5, start.month, start.day)
 
 dates_ahead = []
 
+#Storing differences in all the dates since dividend pay outs aren't the same
 for entry in dividends["data"]:
     dates_ahead.append(str(window - entry["date"]).split(" ")[0])
     print(str(window - entry["date"]))
+
 
 dates_ahead = list(map(lambda x: abs(int(x)), dates_ahead))
 curr_pos = dates_ahead.index(min(dates_ahead))
@@ -37,7 +42,10 @@ print(curr_pos)
 
 bottom_bound = 0
 
-#This means that when we reach the end with our bound of sliding window we will never get another run
+#Note: This means that when we reach the end with our bound of sliding window we will never get another run
+
+
+'''
 while curr_pos < len(dividends["data"]):
     increase = dividends["data"][curr_pos]["value"] / dividends["data"][bottom_bound]["value"]
     print(increase)
@@ -47,11 +55,11 @@ while curr_pos < len(dividends["data"]):
     bottom_bound += 1
     curr_pos += 1
 
-
+'''
 
 print("Pages of dividend", dividends["total_pages"])
 
-'''
+
 total_pages = int(r["total_pages"])
 print(total_pages)
 pages_stored = []
@@ -66,17 +74,24 @@ for page in range(1, 3 + 1):
 print(pages_stored)
 
 #Simple Total return index calculation
+tri = 100
+num_stocks = None
+
 with open('output.csv', 'w') as csvfile:
-    fieldnames = ['stock_price', 'date', 'split_ratio', 'dividend_value']
+    fieldnames = ['Stock Price', 'Date', 'Split Ratio', 'Dividend Value', 'Total Return Index', 'Stocks Owned']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     writer.writeheader()
     for r in pages_stored:
         for i in r["data"]:
             dividend = None
+            if num_stocks == None:
+                num_stocks = tri / i["adj_close"]
             for k in dividends['data']:
                 if i['date'] == k['date']:
                     dividend = k["value"]
-            writer.writerow({'date': i["date"], 'stock_price': i["adj_close"],
-                             'split_ratio': i["split_ratio"], 'dividend_value': dividend})
-'''
+                    num_stocks += dividend / i["adj_close"]
+            tri = num_stocks * i["adj_close"]
+            writer.writerow({'Date': i["date"], 'Stock Price': i["adj_close"],
+                             'Split Ratio': i["split_ratio"], 'Dividend Value': dividend,
+                             'Total Return Index': tri, "Stocks Owned": num_stocks})
