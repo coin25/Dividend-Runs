2ebb254e380c615afa0956cf0c3da94186e7a3ef
coin25 <azzy_coin25@hotmail.com>
2017-08-20 13:36:41 -0700

    Add runs functionality

--- request.py
+++ request.py
@@ -3,13 +3,14 @@ import json
 import csv
 import time
 import datetime
+from pprint import pprint
 from ast import literal_eval
 
 with open("config.txt", "r") as file:
     USER = file.readline()[:-1]
     PASS = file.readline()
 
-url = 'https://api.intrinio.com/historical_data?identifier=AAPL&&sort_order=asc&item=dividend'
+url = 'https://api.intrinio.com/historical_data?identifier=MSFT&sort_order=asc&item=dividend'
 #url = 'https://api.intrinio.com/prices?identifier=AAPL&sort_order=asc&page_number=1'
 s = requests.Session()
 s.auth = (USER, PASS)
@@ -27,34 +28,76 @@ for entry in dividends["data"]:
 
 
 start = dividends["data"][0]["date"]
-window = datetime.date(start.year + 5, start.month, start.day)
+print(start)
 
-dates_ahead = []
+print(start)
+pprint(dividends["data"])
+def dates_next(bottom_bound):
+    start = dividends["data"][bottom_bound]["date"]
+    window = datetime.date(start.year + 5, start.month, start.day)
+    dates_ahead = []
 
-#Storing differences in all the dates since dividend pay outs aren't the same
-for entry in dividends["data"]:
-    dates_ahead.append(str(window - entry["date"]).split(" ")[0])
+    #Storing differences in all the dates since dividend pay outs aren't the same
+    for entry in dividends["data"]:
+        if entry["date"] != window:
+            dates_ahead.append(str(window - entry["date"]).split(" ")[0])
+    dates_ahead = list(map(lambda x: abs(int(x)), dates_ahead))
+    upper_bound = dates_ahead.index(min(dates_ahead))
+    return upper_bound
 
+    #dividends["data"].append(entry)
 
-dates_ahead = list(map(lambda x: abs(int(x)), dates_ahead))
-curr_pos = dates_ahead.index(min(dates_ahead))
-print(curr_pos)
-
+#dividends["data"] = []
+#dividends["data"] = sorted(dividends["data"], key = lambda x: x["date"])
+#print(dividends["data"])
 bottom_bound = 0
+upper_bound = dates_next(bottom_bound)
+print(upper_bound)
+
+#Key is start, value is end
+runs = {}
+
+#All values are in months
+NO_GROWTH_CUTOFF_LENGTH = 36
+NEGATIVE_GROWTH_CUTOFF_LENGTH = 13
 
 #Note: This means that when we reach the end with our bound of sliding window we will never get another run
-'''
-while curr_pos < len(dividends["data"]):
-    increase = dividends["data"][curr_pos]["value"] / dividends["data"][bottom_bound]["value"]
-    print(increase)
-    if increase > 50:
-        tri_start = bottom_bound
+#Also, 1825 days is 5 years
+while bottom_bound < len(dividends["data"]):
+    upper_bound = dates_next(bottom_bound)
+    increase = dividends["data"][upper_bound]["value"] / dividends["data"][bottom_bound]["value"]
+    days_off = abs(datetime.timedelta(1825) - (dividends["data"][upper_bound]["date"] - dividends["data"][bottom_bound]["date"]))
+    print(days_off.days)
+    #This is super arbitrary for days off, we can change this
+    if increase >= 1.5 and days_off.days < 50:
+        run_start = bottom_bound
+        #Add whichever one is lower (should always be negative growth)
+        cutoff_upper_bound = run_start + NEGATIVE_GROWTH_CUTOFF_LENGTH
+        difference = NO_GROWTH_CUTOFF_LENGTH - NEGATIVE_GROWTH_CUTOFF_LENGTH
+        cut = -1
+        print(increase, dividends["data"][upper_bound]["date"] - dividends["data"][bottom_bound]["date"], dividends["data"][bottom_bound]["date"], dividends["data"][upper_bound]["date"])
+        while run_start < len(dividends["data"]):
+            #Problem here: Where do we end with either of these cutoffs? What do we do when we run out of data?
+            #Check negative growth
+            if cutoff_upper_bound < len(dividends["data"]):
+                growth = dividends["data"][cutoff_upper_bound]["value"] / dividends["data"][run_start]["value"]
+                if growth < 1:
+                    #For now we start at bottom
+                    cut = run_start
+            #Check plateau growth
+            if cutoff_upper_bound + difference < len(dividends["data"]):
+                growth = dividends["data"][cutoff_upper_bound + difference]["value"] / dividends["data"][run_start]["value"]
+                if growth <= 1:
+                    cut = run_start
+            if cut == run_start or run_start == len(dividends["data"]) - 1:
+                #If we are making it to the end of the data then we put it in there too
+                runs[dividends["data"][bottom_bound]["date"]] = dividends["data"][run_start]["date"]
+                break
+            run_start += 1
+            cutoff_upper_bound += 1
 
     bottom_bound += 1
-    curr_pos += 1
-
-'''
-
+print(runs)
 print("Pages of dividend", dividends["total_pages"])
 
 
